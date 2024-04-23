9ec19c8121733bb8af9c150197ab9af4f0b29bbd
s22811 <127020803+s22811@users.noreply.github.com>
2024-04-23 17:19:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/skills/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMember',
+    bindToController: true
+  };
+}
